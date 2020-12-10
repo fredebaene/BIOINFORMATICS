1ff403c2e51c3173edb2cd8e2a824938e0848834
fredebaene <frederick.debaene@hotmail.com>
2020-12-10 18:49:40 +0000

    Validate DNA Sequence

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from test import *
 
-a = int(input("Input a first number: "))
-b = int(input("Input a second number: "))
+sequence = input("Please enter your DNA sequence: ")
 
-print(sum(a, b))
-
-example_string = "This is a test of pushing a commit to a GitHub test branch."
-
-print(example_string)
+print(validate_dna_sequence(sequence))
