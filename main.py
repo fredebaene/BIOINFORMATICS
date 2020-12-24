72ac529fd3624a5d60bef044991991b7bbb6009c
fredebaene <frederick.debaene@hotmail.com>
2020-12-24 09:41:05 +0000

    Independent Alleles

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from bioinformatics_toolkit import dna
 from bioinformatics_toolkit import fasta_file_handling
+from bioinformatics_toolkit import probability as prob
 
+k = 7
+n = 37
 
-k = int(input("Enter the generation: "))
-total_number = 2**k
-print(total_number)
-n = int(input("The mimimal number : "))
-print(n)
-probability = 0
-pow = n
-
-for i in range(n, total_number + 1):
-    probability += ((0.25)**i) * ((0.75)**(total_number - i))
-
-print(probability)
+result = round(prob.independent_alleles(k, n), 3)
+print(result)
