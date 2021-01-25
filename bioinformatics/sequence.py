aaf4d0ddc80cf35544d15dde9a6c57bd5d3c3dbd
Frederick De Baene <frederickdebaene@MacBook-Pro-van-Frederick.local>
2021-01-25 21:27:21 +0100

    Creating DNA Class

--- bioinformatics/sequence.py
+++ bioinformatics/sequence.py
@@ -1,21 +1,25 @@
 # SEQUENCE CLASS
 # ----------------------------------------------------------------------------------------------------
-
 class Sequence(object): # a class is a blueprint describing the objects belonging to that class
 
-    def __init__(self, sequence):
+    elements = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" # class variables are shared among all instances of the class
+    sequence_type = "sequence"
 
+    def __init__(self, sequence): # the init method can also be considered as a constructor method
         self.sequence = sequence # attributes are instance variables
+
+    def print_sequence(self): # a method inside of a class automatically takes the instance as its first argument
+        return self.sequence
 
 # APPLICATION
 # ----------------------------------------------------------------------------------------------------
-
 seqOne = Sequence("ATATCGG") # the init method is run automatically whenever a new instance of a class is created
-seqTwo = Sequence("TATCGGTATCGCGCG") # the init method is run automatically whenever a new instance of a class is created
+seqTwo = Sequence("TATCGGTATCGCGCG")
 
-print(seqOne)
-print(seqTwo)
-print(type(seqOne))
-print(type(seqTwo))
-print(seqOne.sequence)
-print(seqTwo.sequence)
+sequence = seqOne.print_sequence()
+
+print(sequence)
+
+sequence = Sequence.print_sequence(seqOne)
+
+print(sequence)
