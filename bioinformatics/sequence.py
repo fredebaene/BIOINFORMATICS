5cf862b262777d2c4aeb099c4ff7a00409a1b939
Frederick De Baene <frederickdebaene@MacBook-Pro-van-Frederick.local>
2021-01-26 08:29:26 +0100

    Complementing a Strand of DNA

--- bioinformatics/sequence.py
+++ bioinformatics/sequence.py
@@ -1,17 +1,22 @@
+# IMPORTING LIBRARIES
+# ----------------------------------------------------------------------------------------------------
+import os
+
 # SEQUENCE CLASS
 # ----------------------------------------------------------------------------------------------------
 class Sequence(object):
 
     monomers = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    sequence_type = "sequence"
 
-    def __init__(self, sequence):
+    def __init__(self, sequence, label = '', description = ''):
 
         for i in sequence:
             if i not in self.monomers:
                 raise ValueError("Invalid Sequence")
 
         self.sequence = sequence
+        self.label = label
+        self.description = description
 
     @property
     def length(self):
@@ -34,12 +39,48 @@ class Sequence(object):
 
         return output[:-1]
 
+    @classmethod
+    def fasta(cls, file):
+
+        with open(file, 'r') as f:
+            lines_of_text = f.readlines()
+
+        lines_of_text.append('$\n')
+        sequences = []
+        label, description, sequence = '', '', ''
+
+        for i in range(len(lines_of_text)):
+
+            if lines_of_text[i][0] == '$':
+
+                break
+
+            elif lines_of_text[i + 1][0] == '>' or lines_of_text[i + 1][0] == '$':
+
+                sequences.append(cls(sequence = sequence, label = label, description = description))
+                label, description, sequence = '', '', ''
+
+            elif lines_of_text[i][0] == '>':
+
+                label_description = lines_of_text[i].split(' ', 1)
+
+                if len(label_description) > 1:
+                    label = label_description[0][1:]
+                    description = label_description[1][:-1]
+                else:
+                    label = label_description[0][1:-1]
+
+            else:
+
+                sequence += lines_of_text[i][:-1]
+
+        return sequences
+
 # DNA CLASS
 # ----------------------------------------------------------------------------------------------------
 class DNA(Sequence):
 
     monomers = "ACGT"
-    sequence_type = "DNA"
 
     def transcribe_as_coding_strand(self):
 
@@ -53,12 +94,21 @@ class DNA(Sequence):
 
         return RNA(rna)
 
+    def reverse_complement(self):
+
+        complementary_bases = {'A' : 'T', 'C' : 'G', 'G' : 'C', 'T' : 'A'}
+        reverse_complement = ''
+
+        for i in self.sequence[::-1]:
+            reverse_complement += complementary_bases[i]
+
+        return reverse_complement
+
 # RNA CLASS
 # ----------------------------------------------------------------------------------------------------
 class RNA(Sequence):
 
     monomers = "ACGU"
-    sequence_type = "RNA"
 
 # APPLICATION
 # ----------------------------------------------------------------------------------------------------
@@ -67,8 +117,5 @@ if __name__ == "__main__":
     # ask user for sequence
     sequence = input("Please enter your sequence : ")
 
-    # sequence analysis
-    seqOne = DNA(sequence)
-    seqTwo = seqOne.transcribe_as_coding_strand()
-    print(type(seqTwo))
-    print(seqTwo.sequence)
+    seqOne = DNA(sequence = sequence)
+    print(seqOne.reverse_complement())
